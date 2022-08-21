319b6f370cc27294d6bff316476ff44b867c32a6
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-21 17:34:39 +0100

    implement e2e test scénario 2

--- tests/2022_08_20_17_10_guest_test.js
+++ tests/2022_08_20_17_10_guest_test.js
@@ -15,4 +15,22 @@ describe('Je suis un visiteur non connecté', function() {
       .end();
     })
   })
+
+  describe('Je remplis le champ e-mail du login administrateur au mauvais format et je clique sur le bouton se connecter', function() {
+    it('Je reste sur la page Login et je suis invité à remplir le champ e-mail au bon format', function(browser) {
+      browser
+      .url('http://127.0.0.1:8080/')
+      .waitForElementVisible('body')
+      .setValue('input[data-testid="admin-email-input"]', 'admin.email.com')
+      .click('button[data-testid="admin-login-button"]')
+      .execute(function() {
+        return document.querySelector('[data-testid="form-admin"]').checkValidity()
+      }, [], function(result) {
+        browser.verify.equal(result.value, false).
+        assert.titleContains('Billed')
+      })
+      .pause(1000)
+      .end();
+    })
+  })
 });
